ee3e0b7dead756d6de040b4e84e34b150fc9ff9c
SpecTEviL <vishalpatil.imp@gmail.com>
2021-03-20 19:02:51 +0530

    20th March 2021 - Hackerrank Solution

--- Hackerrank/44.Classes and Objects.cpp
+++ Hackerrank/44.Classes and Objects.cpp
@@ -1,26 +1,25 @@
 /*
-    https://www.hackerrank.com/challenges/c-tutorial-functions/problem
+    https://www.hackerrank.com/challenges/classes-objects/problem
 */
 
-#include <iostream>
-using namespace std;
 
-int max_of_four(int a, int b, int c, int d){
-    int arr[4] = {a , b , c , d};
-    int max = arr[0];
-    for(int i = 1 ; i < 4 ; i++){
-        if(arr[i] > max){
-            max = arr[i];
+
+class Student{
+    public:
+        int scores[5];
+
+        void input(){
+            for(int i = 0; i < 5 ; i++){
+                cin >> scores[i];
+            }
         }
-    }
-    return max;
-}
+        int calculateTotalScore(){
+            int sum = 0;
+            for(int i = 0; i < 5 ; i++){
+                sum += scores[i];
+            }
+        return sum;
+        }
+};
 
-int main(){
-    int a, b, c, d;
-    scanf("%d %d %d %d", &a, &b, &c, &d);
-    int ans = max_of_four(a, b, c, d);
-    printf("%d", ans);
-    
-    return 0;
-}
+
