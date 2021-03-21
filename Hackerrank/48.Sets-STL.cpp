3f897445ea69ab364f25ae110d9d9484475a864e
SpecTEviL <vishalpatil.imp@gmail.com>
2021-03-21 19:15:17 +0530

    21st March 2021 - Hackerrank Solution

--- Hackerrank/48.Sets-STL.cpp
+++ Hackerrank/48.Sets-STL.cpp
@@ -1,25 +1,38 @@
 /*
-    https://www.hackerrank.com/challenges/classes-objects/problem
+    https://www.hackerrank.com/challenges/cpp-sets/problem
 */
 
+#include <bits/stdc++.h>
 
+using namespace std;
 
-class Student{
-    public:
-        int scores[5];
-
-        void input(){
-            for(int i = 0; i < 5 ; i++){
-                cin >> scores[i];
+int main() {
+    int q;
+    cin >> q;
+    vector<string> ans ;
+    set<int> s;
+    for (int i=0 ; i < q ; i++){
+        int y, x;
+        cin >> y >> x;
+        if (y == 1){
+            s.insert(x);
+        }
+        else if (y == 2){
+            s.erase(x);
+        }
+        else if (y == 3){
+            set<int>::iterator itr=s.find(x);
+            if (itr != s.end()){
+                ans.push_back("Yes");
+            }
+            else{
+                ans.push_back("No");
             }
         }
-        int calculateTotalScore(){
-            int sum = 0;
-            for(int i = 0; i < 5 ; i++){
-                sum += scores[i];
-            }
-        return sum;
+    }
+    for (string x: ans){
+            cout << x <<endl;
         }
-};
-
+    return 0;
+}
 
